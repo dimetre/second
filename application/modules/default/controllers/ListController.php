dca97da11592b603f49aa504e53dedbaa5f075d1
vladimir <vladimirdemetrebalan@gmail.com>
2016-08-13 16:58:54 +0300

    add+edit form v0.1

--- application/modules/default/controllers/ListController.php
+++ application/modules/default/controllers/ListController.php
@@ -17,14 +17,34 @@ class ListController extends Zend_Controller_Action{
     	$admin=new Zend_Session_Namespace('admin');
     	if(!isset($admin->name)){
     		$this->_helper->redirector->gotoUrl('/');
+    		die;
     	}
+    	// remove fom db and shit
+
     }
     public function addAction(){
-    	$admin=new Zend_Session_Namespace('admin');
+    	$admin = new Zend_Session_Namespace('admin');
     	if(!isset($admin->name)){
     		$this->_helper->redirector->gotoUrl('/');
+    		die;
     	}
-    	$this->view->form = $form = new Application_Forms_Edit();
+    	$form = new Application_Forms_Edit();
+    	if($this->getRequest()->getParam('id') !== null){
+    		$id = $this->getRequest()->getParam('id');
+    		$db = Zend_Db_Table::getDefaultAdapter();
+    		$select = $db->select();
+    		$select->from('products')->where('id = ?',$id);
+    		$stm = $db->query($select);
+            $result = $stm->fetchAll();
+            $form->getElement('title')->setValue($result [0]['title']);
+            $form->getElement('price')->setValue($result [0]['price']);
+            $form->getElement('description')->setValue($result [0]['description']);
+            $form->getElement('image')->setValue($result [0]['image']);
+    	}
+    	$this->view->form = $form;
+    	if($this=>getRequest()->getParam('save')!==null && $this=>getRequest()->getParam('title')!==null){
+    		
+    	}
     }
 }
 
