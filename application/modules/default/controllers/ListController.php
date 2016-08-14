6d599f6e68d5fc8584398eea776710288c8412e3
vladimir <vladimirdemetrebalan@gmail.com>
2016-08-14 15:43:34 +0300

    edit + remove +add v1.0 (image remaining)

--- application/modules/default/controllers/ListController.php
+++ application/modules/default/controllers/ListController.php
@@ -15,15 +15,21 @@ class ListController extends Zend_Controller_Action{
     }
     public function removeAction(){
     	$admin=new Zend_Session_Namespace('admin');
+    	$db=Zend_Db_Table::getDefaultAdapter();
     	if(!isset($admin->name)){
     		$this->_helper->redirector->gotoUrl('/');
     		die;
     	}
-    	// remove fom db and shit
+    	if($this->getRequest()->getParam('id')!==null){
+    		$db->query('DELETE FROM products WHERE id = ?',$this->getRequest()->getParam('id'));
+    	}
+    	$this->_helper->redirector->gotoUrl('/list');
+    	// image
 
     }
     public function addAction(){
     	$admin = new Zend_Session_Namespace('admin');
+    	$db = Zend_Db_Table::getDefaultAdapter();
     	if(!isset($admin->name)){
     		$this->_helper->redirector->gotoUrl('/');
     		die;
@@ -31,7 +37,6 @@ class ListController extends Zend_Controller_Action{
     	$form = new Application_Forms_Edit();
     	if($this->getRequest()->getParam('id') !== null){
     		$id = $this->getRequest()->getParam('id');
-    		$db = Zend_Db_Table::getDefaultAdapter();
     		$select = $db->select();
     		$select->from('products')->where('id = ?',$id);
     		$stm = $db->query($select);
@@ -40,11 +45,24 @@ class ListController extends Zend_Controller_Action{
             $form->getElement('price')->setValue($result [0]['price']);
             $form->getElement('description')->setValue($result [0]['description']);
             $form->getElement('image')->setValue($result [0]['image']);
-    	}
+            $lastid = $result[0]['id'];
+    	} 
     	$this->view->form = $form;
-    	if($this=>getRequest()->getParam('save')!==null && $this=>getRequest()->getParam('title')!==null){
-    		
+    	if($this->getRequest()->getParam('save')!==null && $this->getRequest()->getParam('title')!==''){
+    		if(!isset($lastid)){
+    			$db->query('INSERT INTO products() VALUES()');
+    			$lastid = $db->lastInsertId();
+            	echo 'last id is '.$lastid;
+    		}
+    		$data = array( 'title'=>$this->getRequest()->getParam('title'),
+							'price'=>$this->getRequest()->getParam('price'),
+							'description'=>$this->getRequest()->getParam('description'),
+							'image'=>$this->getRequest()->getParam('image'));
+    		$db->update('products',$data,'id ='.$lastid);
+    		$this->_helper->redirector->gotoUrl('/list');
     	}
+    	//image
+    	
     }
 }
 
